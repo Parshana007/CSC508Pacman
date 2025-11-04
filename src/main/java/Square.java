533745cd9dc701ae1ccbcc0bb89281069dc5b36f
Mio <manakaga@calpoly.edu>
2025-11-03 20:58:02 -0800

    Subscriber, Square added

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,3 +1,36 @@
 public class Square {
     // Should have an x, y, color??
+    private int x;
+    private int y;
+    private String color;
+
+    public Square(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
